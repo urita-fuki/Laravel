07a98473865d2f1d9960b9183192a7fa23ca1766
urita-fuki <nogihina0809@i.softbank.jp>
2022-03-01 12:44:24 +0900

    3/1 2日目

---
+++ laravelapp/resources/views/hello/index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+      <title>Hello/Index</title>
+        <style>
+          body {font-size:16pt; color:#999;}
+          h1 {font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px;}
+        </style>
+    </head>
+    <body>
+      <h1>Index</h1>
+      <p><?php echo $msg; ?></p>
+      <p><?php echo date("Y年n月日"); ?></p>
+    </body>
+</html>
+EOF;  
